6513b3112259ed0d23f904ddf5e47c4ce7e07655
Alisha-IM05 <alisha.im@outlook.com>
2025-10-20 14:56:14 -0400

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-# 1. Initialize the FastAPI app
-app = FastAPI()
-
-# 2. Define your first endpoint (a test route)
-@app.get("/")
-def read_root():
-    return {"Hello": "Restaurant Ordering Backend"}
